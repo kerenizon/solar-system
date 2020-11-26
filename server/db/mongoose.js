69cb9b03e8239676c0fe97602206a3bddb11ffbc
kerenizon <keren.izon@gmail.com>
2020-11-26 12:48:35 +0200

    changed files

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/solar-system', {
+// mongoose.connect('mongodb://127.0.0.1:27017/solar-system', {
+mongoose.connect('process.env.MONGO_URI', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
